188341e7d532f5721093b9549da0f23a62759702
Jak1022 <70177860+Jak1022@users.noreply.github.com>
2020-11-02 12:57:02 -0500

    update 20nov02

---
+++ 20oct15/choice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+choice=input("Do you want to center by mass or geometry")
+
+if choice=="M":
+	#center by mass
+if choice=="G":
+	#center by geometry
+	
---
+++ 20oct29/global.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+x=40
+
+def f1():
+	global x
+	x=80
+	print(x)
+
+def f2():
+	global x
+	x=160
+	print(x)
+
+print(x)
+f2()
+f1()
+print(x)
