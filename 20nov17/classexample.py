8fb93f7ebae7e48eb3a965a6b16a19c20b8d6c94
Jak1022 <70177860+Jak1022@users.noreply.github.com>
2020-12-04 15:57:16 -0500

    update

---
+++ 20nov17/classexample.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+class C1:
+	def __init__(self,value=1):
+		self.data=value
+	def setdata(self,data):
+		self.data=value
+	def display(pickle):
+		print("The data is", pickle.data)
+		
+class C2(C1):
+	def setdata2(self,value):
+		self.data2=value
---
+++ 20nov17/pyplotexample.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import numpy
+from matplotlib import pyplot
+
+pyplot.figure(figsize=(10,10), dpi=60)
+pyplot.subplot(2,2,1)
+
+X=numpy.linspace(-numpy.pi,numpy.pi, 256, endpoint=True)
+C=numpy.cos(X)
+S=numpy.sin(X)
+
+pyplot.plot(X,C, color="blue", linewidth=1.0, linestyle="-")
+pyplot.plot(X,S, color="green", linewidth=1.0, linestyle="-")
+
+
+pyplot.subplot(2,2,2)
+pyplot.plot(X,S, color="red", linewidth=2.0, linestyle="--")
+
+pyplot.subplot(2,2,3)
+pyplot.plot(X,S, color="black", linewidth=2.0, linestyle="--")
+
+pyplot.subplot(2,2,4)
+pyplot.plot(X,S, color="violet", linewidth=2.0, linestyle="--")
+
+pyplot.xlim(-4,4)
+pyplot.ylim(-2,2)
+pyplot.xticks(numpy.linspace(-4,4,9, endpoint=True))
+pyplot.yticks(numpy.linspace(-2,2,5, endpoint=True))
+
+pyplot.savefig("sines.png", dpi=144)
+#pyplot.show()
